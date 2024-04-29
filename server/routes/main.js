1d313e9fb0f8942f4b8905010a7d930817d0fcdf
Richard Maceda <richardmaceda204@gmail.com>
2024-04-28 18:07:41 -0700

    pass a locals dictionary for index router and also made a router for about page

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 //Routes
 router.get('', (req, res) => {  //inside the '' if I put /about it will be the about page
-    res.send("Hello World");
+    
+    const locals = {
+        title: 'NodeJs Blog',
+        description: 'Simple log created with NodeJs, ExpressJs and MongoDB'
+    }    
+    
+    res.render('index', {locals}); //renders the index.ejs file and passed the locals object (I can pass multiple objects with the {} )
 });
 
-module.exports = router;
+router.get('/about', (req, res) => {  //type localhost:5000/about, note that I added '/' 
+    res.render('about'); //renders the index.ejs file
+});
+
+
+
+module.exports = router; //allows other files to use the router
