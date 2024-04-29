9b889f9d70f8ca5c2bccb88f679c4c91d8daa215
Richard Maceda <richardmaceda204@gmail.com>
2024-04-28 23:43:48 -0700

    add mongoose and use connectDB function

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ require('dotenv').config(); //So we can use the .env file
 const express = require('express'); // We can now use the express variable to create an express ap
 const expressLayout = require('express-ejs-layouts'); // Including the express - ejs layout
 
+const connectDB = require('./server/config/db'); // We require the db.js file
+
 const app = express(); // We create an express app
 const PORT = 5000 || process.env.PORT; // We set the port to 5000 or the port in the .env file
 
+
+//connect to Database
+connectDB(); // We call the connectDB function to connect to the database
 
 app.use(express.static('public'));
 
